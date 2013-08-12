ed09fc6388959ba147e8f563ed9e64b953f5056f
browning <bwbrowning@gmail.com>
2013-08-12 05:31:45 -0500

    basic showlist view complete

--- shows/showlist/views.py
+++ shows/showlist/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.http import Http404
+from django.shortcuts import render_to_response
+from showlist.models import Show
+
+def index(request):
+    shows = Show.objects.all()
+    return render_to_response('showlist/index.html', {'shows': shows})
--- shows/shows/urls.py
+++ shows/shows/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from showlist import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^$', 'showlist.views.index'),
 
     # Examples:
     # url(r'^$', 'shows.views.home', name='home'),
