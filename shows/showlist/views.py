334cc466438ed6fd9d69c298499d755db27f7c52
browning <bwbrowning@gmail.com>
2013-08-12 21:27:28 -0500

    organized show index template to group by date and display multiple tables

--- shows/showlist/views.py
+++ shows/showlist/views.py
@@ -2,9 +2,19 @@ from django.http import Http404
 from django.shortcuts import render_to_response
 from showlist.models import Show
 import datetime
+from collections import OrderedDict
+import pytz
 
 def index(request):
 	# get a list of shows from yesterday to 2 weeks in the future
     shows = Show.objects.filter(datetime__gte=datetime.date.today() + datetime.timedelta(days=-1),
-    	datetime__lte=datetime.date.today() +datetime.timedelta(days=14))
-    return render_to_response('showlist/index.html', {'shows': shows})
+    	datetime__lte=datetime.date.today() +datetime.timedelta(days=14)).order_by('datetime')
+    showlist = OrderedDict()
+    for show in shows:
+    	md = pytz.timezone("America/New_York").normalize(show.datetime).strftime("%A %m/%d")
+    	if str(md) in showlist:
+    		showlist[md].append(show)
+    	else:
+    		showlist[md] = [show]
+    print "%r" % showlist
+    return render_to_response('showlist/index.html', {'shows': showlist})
