c4d17223544ecd3462f10b44c8a8e300c40c6ce7
browning <bwbrowning@gmail.com>
2013-08-12 19:54:33 -0500

    limit index to shows from yesterday to 2 weeks in the future

--- shows/showlist/views.py
+++ shows/showlist/views.py
@@ -1,7 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render_to_response
 from showlist.models import Show
+import datetime
 
 def index(request):
-    shows = Show.objects.all()
+	# get a list of shows from yesterday to 2 weeks in the future
+    shows = Show.objects.filter(datetime__gte=datetime.date.today() + datetime.timedelta(days=-1),
+    	datetime__lte=datetime.date.today() +datetime.timedelta(days=14))
     return render_to_response('showlist/index.html', {'shows': shows})
