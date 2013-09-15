9739c783953fed9ae7f5813bf9e512dd649640be
browning <bwbrowning@gmail.com>
2013-09-15 17:16:51 -0500

    Add band model to the admin site

--- shows/showlist/admin.py
+++ shows/showlist/admin.py
@@ -1,6 +1,7 @@
-from models import Genre, Venue, Show
+from models import Genre, Venue, Show, Band
 from django.contrib import admin
 
 admin.site.register(Genre)
 admin.site.register(Venue)
+admin.site.register(Band)
 admin.site.register(Show)
